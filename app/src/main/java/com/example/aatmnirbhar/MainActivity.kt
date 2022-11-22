e3c3c03989afe31a08f286e1480c05f203501e8d
vishnu5606 <vishnumohan5606@gmail.com>
2022-11-22 18:24:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/aatmnirbhar/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.aatmnirbhar
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
