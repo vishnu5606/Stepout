8b51556e91e37ebd6fd2c60af8aa14a23f18c540
vishnu5606 <vishnumohan5606@gmail.com>
2022-11-23 18:51:55 +0530

    Initial Commit

--- app/src/main/java/com/example/aatmnirbhar/MainActivity.kt
+++ app/src/main/java/com/example/aatmnirbhar/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.aatmnirbhar
 
+import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signupButton = findViewById<Button>(R.id.signup)
+        signupButton.setOnClickListener {
+            val Intent = Intent(this,signin_page::class.java)
+            startActivity(Intent)
+        }
 
     }
 
---
+++ app/src/main/java/com/example/aatmnirbhar/signin_page.kt
@@ -0,0 +1,11 @@
+package com.example.aatmnirbhar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signin_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_page)
+    }
+}
