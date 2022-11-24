34e34bbd966a5c4c9535c8d99dc34fcd3676a1e9
vishnu5606 <vishnumohan5606@gmail.com>
2022-11-24 23:34:19 +0530

    third commit

---
+++ app/src/main/java/com/example/aatmnirbhar/forgotpassword.kt
@@ -0,0 +1,14 @@
+package com.example.aatmnirbhar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgotpassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgotpassword)
+
+
+
+    }
+}
