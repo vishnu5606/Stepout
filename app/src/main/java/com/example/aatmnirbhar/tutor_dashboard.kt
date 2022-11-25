39fa3e94365f0c589dfc0e73149daa06885c6d2c
vishnu5606 <vishnumohan5606@gmail.com>
2022-11-25 17:47:03 +0530

    fourth commit

---
+++ app/src/main/java/com/example/aatmnirbhar/tutor_dashboard.kt
@@ -0,0 +1,11 @@
+package com.example.aatmnirbhar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tutor_dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_dashboard)
+    }
+}
