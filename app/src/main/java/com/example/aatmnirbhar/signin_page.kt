a734612499b603a2b0f8dbb19991a27c8bce809b
vishnu5606 <vishnumohan5606@gmail.com>
2022-11-27 21:51:52 +0530

    sixth commit

--- app/src/main/java/com/example/aatmnirbhar/signin_page.kt
+++ app/src/main/java/com/example/aatmnirbhar/signin_page.kt
@@ -1,11 +1,4 @@
 package com.example.aatmnirbhar
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class signin_page : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin_page)
-    }
+class signin_page {
 }
